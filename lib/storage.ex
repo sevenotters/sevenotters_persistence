5a2f0c1b7dcbecf79c2cee16588e78e53def7754
“nicolafiorillo” <nicola.fiorillo@gmail.com>
2020-04-23 23:28:47 +0200

    Refactoring.

--- lib/storage.ex
+++ lib/storage.ex
@@ -1,4 +1,6 @@
 defmodule SevenottersPersistence.Storage do
+  @moduledoc false
+
   @callback initialize(String.t()) :: any
   @callback insert(String.t(), Map.t()) :: any
   @callback new_id :: any
@@ -6,9 +8,11 @@ defmodule SevenottersPersistence.Storage do
   @callback printable_id(any) :: String.t()
   @callback is_valid_id?(any) :: Boolean.t()
   @callback max_in_collection(String.t(), atom) :: Int.t()
-  @callback content_of(String.t(), Map.t(), Map.t()) :: List.t()
   @callback drop_collections(List.t()) :: any
-  @callback sort_expression() :: any
-  @callback correlation_id_expression() :: any
-  @callback type_expression([String.t()]) :: any
+
+  @callback content_by_correlation_id(String.t(), String.t(), any) :: List.t()
+  @callback content_by_types(String.t(), [String.t()], any) :: List.t()
+  @callback content(String.t()) :: List.t()
+
+  @callback sort_expression() :: atom()
 end
