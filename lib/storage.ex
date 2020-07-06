126a41ea0075c7c9bf91c5685ed7050b0bd2b6e2
Nicola Fiorillo <nicola.fiorillo@gmail.com>
2020-07-06 16:01:07 +0200

    Adding new printable id.. Bump to 0.1.2.

--- lib/storage.ex
+++ lib/storage.ex
@@ -1,18 +1,19 @@
 defmodule SevenottersPersistence.Storage do
   @moduledoc false
 
-  @callback initialize(String.t()) :: any
-  @callback insert(String.t(), Map.t()) :: any
+  @callback initialize(bitstring) :: any
+  @callback insert(bitstring, map) :: any
   @callback new_id :: any
-  @callback object_id(String.t()) :: any
-  @callback printable_id(any) :: String.t()
-  @callback is_valid_id?(any) :: Boolean.t()
-  @callback max_in_collection(String.t(), atom) :: Int.t()
-  @callback drop_collections(List.t()) :: any
+  @callback new_printable_id :: bitstring
+  @callback object_id(bitstring) :: any
+  @callback printable_id(any) :: bitstring
+  @callback is_valid_id?(any) :: boolean
+  @callback max_in_collection(bitstring, atom) :: integer
+  @callback drop_collections([bitstring]) :: any
 
-  @callback content_by_correlation_id(String.t(), String.t(), any) :: List.t()
-  @callback content_by_types(String.t(), [String.t()], any) :: List.t()
-  @callback content(String.t()) :: List.t()
+  @callback content_by_correlation_id(bitstring, bitstring, any) :: [map]
+  @callback content_by_types(bitstring, [bitstring], any) :: [map]
+  @callback content(bitstring) :: [map]
 
-  @callback sort_expression() :: atom()
+  @callback sort_expression() :: atom
 end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SevenottersPersistence.MixProject do
   def project do
     [
       app: :sevenotters_persistence,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
